bf8328092660fb09286f7fe97f587c683cccd0bb
Murugan47 <Nezarkarate@gmail.com>
2025-08-28 20:41:45 -0400

    Created popups
    Both the about and tutorial buttons are now fully functional as pop ups that explain to the user how to play the game and also why this game is being made

---
+++ main.js
@@ -0,0 +1,11 @@
+function myfunction() 
+{
+    var popup = document.getElementById("tutorialpopup");
+    popup.classList.toggle("show");
+}
+
+function myfunctiontwo() 
+{
+    var popup = document.getElementById("aboutpopup");
+    popup.classList.toggle("show");
+}
