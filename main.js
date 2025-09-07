42f39ba017f740a93bbdd585a93e61e493401ecc
boredomu <bennyhe.bh@gmail.com>
2025-09-07 17:16:56 -0400

    testing
    testing

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ function myfunctiontwo()
     var popup = document.getElementById("aboutpopup");
     popup.classList.toggle("show");
 }
+
+async function youtubeRequest()
+{
+    var api_url = "https://youtube.googleapis.com/youtube/v3/videos?key=[AIzaSyDnepoj7VlCv5QY9oeM1vOY2vpCUIv5zqQ]"
+    try
+    {
+        print("ten thousand")
+        var fetchResponse = await fetch(api_url);
+        const result = await response.json();
+        console.log(result);
+    }
+    catch
+    {
+        print("my pc");
+    }
+}
